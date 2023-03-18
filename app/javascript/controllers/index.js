1cebce36cff81201bd45b8f8c579e3bec7bc76fe
Michael Nelin <mishanelin17@gmail.com>
2023-03-18 10:41:35 +0100

    stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import EditIngredientController from "./edit_ingredient_controller"
+application.register("edit-ingredient", EditIngredientController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
--- app/javascript/controllers/show_filters_controller.js
+++ app/javascript/controllers/show_filters_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="show-filters"
 export default class extends Controller {
   static targets = ["button"]
-
+  
   displayForm() {
     this.buttonTarget.classList.toggle("d-none")
   }
