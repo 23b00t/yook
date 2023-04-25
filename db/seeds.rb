ce7af19b38098360726e2b86fd50107c1a6f4997
Daniel Kipp <daniel.kipp@gmail.com>
2023-04-25 13:43:11 +0200

    fixed link

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,6 @@ RecipeService.new(user).create_with_scrape(link: "https://www.allrecipes.com/rec
 RecipeService.new(user).create_with_scrape(link: "https://www.allrecipes.com/recipe/8887/chicken-marsala/")
 RecipeService.new(user).create_with_scrape(link: "https://www.allrecipes.com/recipe/15925/creamy-au-gratin-potatoes/")
 RecipeService.new(user).create_with_scrape(link: "https://www.allrecipes.com/recipe/282286/easy-veggie-pasta-primavera/")
-RecipeService.new(user).create_with_scrape(link: "allrecipes.com/recipe/16947/amazingly-easy-irish-soda-bread/")
+RecipeService.new(user).create_with_scrape(link: "https://www.allrecipes.com/recipe/16947/amazingly-easy-irish-soda-bread/")
 
 p 'All done'
